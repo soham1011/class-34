fb62877670fb6b701902a700cccd54124e24414f
soham1011 <72206422+soham1011@users.noreply.github.com>
2021-08-29 12:59:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine,world;
+var ball;
+function setup() {
+  createCanvas(3000,800);
+ 
+  engine=Engine.create()
+    world=engine.world;
+    ball= new Ball(200,200,80);
+    slingshot = new SlingShot(ball.body,{x:500, y:250});
+    box1 = new Box(900,100,70,70);
+    box2 = new Box(900,100,70,70);
+    box3 = new Box(900,100,70,70);
+    box4 = new Box(900,100,70,70);
+    box5 = new Box(900,100,70,70);
+    box6 = new Box(800,100,70,70);
+    box7 = new Box(800,100,70,70);
+    box8 = new Box(800,100,70,70);
+    box9 = new Box(800,100,70,70);
+    box10 = new Box(800,100,70,70);
+    box11 = new Box(800,100,70,70);
+    box12 = new Box(800,100,70,70);
+    box13 = new Box(800,100,70,70);
+    box14 = new Box(800,100,70,70);
+    
+    box15 = new Box(700,100,70,70);
+    box16 = new Box(700,100,70,70);
+    box17 = new Box(700,100,70,70);
+    box18 = new Box(700,100,70,70);
+    ground=new Ground(1500,780,3000,30);
+}
+
+function draw() {
+  background(255,255,255);  
+  Engine.update(engine);
+  slingshot.display();
+  ball.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display();
+  box9.display();
+  box10.display();
+  box11.display();
+  box12.display();
+  box13.display();
+  box14.display();
+  box15.display();
+  box16.display();
+  box17.display();
+  box18.display();
+  ground.display();
+  drawSprites();
+  
+}
